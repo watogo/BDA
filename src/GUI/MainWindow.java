74b5bb3ac3659faa20680820aec0773112e0cb2e
hizonomi <niklausmanser@hotmail.com>
2016-05-02 15:32:00 +0200

    Added scheduling functionality

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -13,6 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.*;
 import Utils.ResponseParser;
+import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 /**
@@ -53,6 +54,12 @@ public class MainWindow extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         resultArea = new javax.swing.JTextArea();
         jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        runSpiderBtn = new javax.swing.JButton();
+        spiderNameField = new javax.swing.JTextField();
+        spiderMessageLabel = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        returnTestArea = new javax.swing.JTextArea();
         selectedProjectLabel = new javax.swing.JLabel();
         apiKeyLabel = new javax.swing.JLabel();
 
@@ -111,15 +118,60 @@ public class MainWindow extends javax.swing.JFrame {
 
         projectsAndSpidersPanel.addTab("List Projects and Spiders", jPanel1);
 
+        runSpiderBtn.setText("Run Spider now");
+        runSpiderBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                runSpiderBtnActionPerformed(evt);
+            }
+        });
+
+        spiderNameField.setText("Enter Spider to be scheduled");
+
+        returnTestArea.setColumns(20);
+        returnTestArea.setRows(5);
+        jScrollPane3.setViewportView(returnTestArea);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel3Layout.createSequentialGroup()
+                                .addGap(241, 241, 241)
+                                .addComponent(spiderMessageLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 73, Short.MAX_VALUE))
+                            .addComponent(spiderNameField))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(runSpiderBtn)
+                        .addGap(185, 185, 185))))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(runSpiderBtn)
+                    .addComponent(spiderNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(89, 89, 89)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(spiderMessageLabel)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 622, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 400, Short.MAX_VALUE)
         );
 
         projectsAndSpidersPanel.addTab("Schedule spiders", jPanel2);
@@ -192,18 +244,46 @@ public class MainWindow extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_listResultsBtnActionPerformed
 
+    private void runSpiderBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_runSpiderBtnActionPerformed
+        try {   
+            Process p;
+            String cmd = "curl -u " + this.key + ": https://dash.scrapinghub.com/api/run.json -d project=" 
+                    + this.projectId + " -d spider=" + this.spiderNameField.getText();
+            System.out.println(cmd);
+            p = Runtime.getRuntime().exec(cmd);
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+             while(true) {
+                    String s = br.readLine();
+                    if(s == null)
+                        break;
+                    String oldContent = returnTestArea.getText();
+                    returnTestArea.setText(oldContent + "\n" + s);
+                }
+            //spiderMessageLabel.setText(s);
+        }   catch (Exception ex) {
+            Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }//GEN-LAST:event_runSpiderBtnActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel apiKeyLabel;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JButton listResultsBtn;
     private javax.swing.JButton listSpidersBtn;
     private javax.swing.JTabbedPane projectsAndSpidersPanel;
     private javax.swing.JTextArea resultArea;
+    private javax.swing.JTextArea returnTestArea;
+    private javax.swing.JButton runSpiderBtn;
     private javax.swing.JLabel selectedProjectLabel;
+    private javax.swing.JLabel spiderMessageLabel;
+    private javax.swing.JTextField spiderNameField;
     private javax.swing.JList spidersList;
     // End of variables declaration//GEN-END:variables
 }
